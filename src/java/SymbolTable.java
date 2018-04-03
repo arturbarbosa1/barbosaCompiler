c37f8693e7e9f20aab652eba687838371908c68a
arturbarbosa1 <35673399+arturbarbosa1@users.noreply.github.com>
2018-04-03 00:20:46 -0400

    Update SymbolTable.java
    This class will implement the symbol table for the semantic analysis

--- src/java/SymbolTable.java
+++ src/java/SymbolTable.java
@@ -1,1 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
 
+public class SymbolTable {
+
+	private Map<String, Entry> symbols;
+	
+	public SymbolTable() {
+		symbols = new HashMap<String, Entry>();
+	}
+	
+	public void addEntry(Entry entry) {
+		symbols.put(entry.name, entry);
+	}
+	
+	public boolean contains(String entryName) {
+		return symbols.containsKey(entryName);
+	}
