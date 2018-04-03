37a3951a2ff9fb4ee5e0bfacba34f684e6d34a37
arturbarbosa1 <35673399+arturbarbosa1@users.noreply.github.com>
2018-04-03 17:15:28 -0400

    Update AST.java

--- src/java/AST.java
+++ src/java/AST.java
@@ -7,3 +7,27 @@ import java.util.List;
 abstract public class AST {	
 	abstract void print(int indentation);
 }
+
+class Block extends Statement{		
+	private List<Statement> statements;
+	
+	public Block() {
+		statements = new ArrayList<Statement>();
+	}
+	public void addStatement(Statement stmt) {
+		statements.add(stmt);
+	}	
+	public List<Statement> getStatements() {
+		return statements;
+	}
+	@Override
+	void print(int indentation) {
+		for(int i=0; i < indentation; i++)
+			System.out.print("-");
+		System.out.println("< BLOCK >");
+		for(Statement stmt : statements)
+			stmt.print(indentation + 1);
+	}	
+}
+
+
