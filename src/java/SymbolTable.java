b533b0cc9cd911c5cd6a78eb182302b05bfe46a9
arturbarbosa1 <35673399+arturbarbosa1@users.noreply.github.com>
2018-04-03 00:42:45 -0400

    Update SymbolTable.java
    symbol table print outs

--- src/java/SymbolTable.java
+++ src/java/SymbolTable.java
@@ -16,3 +16,14 @@ public class SymbolTable {
 	public boolean contains(String entryName) {
 		return symbols.containsKey(entryName);
 	}
+	//table
+	public void print() {
+		System.out.println("--------------------------");
+		System.out.println("Name Type      Scope  Line");
+		System.out.println("--------------------------");
+		for(String name : symbols.keySet()) {
+			Entry e = symbols.get(name);
+			System.out.println(name+"    " + e.getType().getLexeme()+"     " + e.getScope() + "  " + e.getType().getLineNo());
+		}
+	}
+}
