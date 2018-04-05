29ad17ed47feb6881f25d76c35538f2b271fdc56
arturbarbosa1 <35673399+arturbarbosa1@users.noreply.github.com>
2018-04-04 23:40:42 -0400

    Update SemanticAnalyser.java

--- src/java/SemanticAnalyser.java
+++ src/java/SemanticAnalyser.java
@@ -7,4 +7,12 @@ import java.util.Stack;
  * This class implements the semantic analysis based on abstract syntax tree
  *
  */
-public class SemanticAnalyser 
+public class SemanticAnalyser {
+  
+ 	private AST ast;
+	private int numErrors;
+	SymbolTable globalSymTable = new SymbolTable();
+	SymbolTable currSymTable = new SymbolTable();
+	Stack<Integer> scopeStack = new Stack<Integer>();
+	Stack<SymbolTable> stackSymTable = new Stack<SymbolTable>();
+	List<Entry> entries = new ArrayList<Entry>();
