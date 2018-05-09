d23cd0d0b13926ac6879a87e718f59befc335b35
arturbarbosa1 <35673399+arturbarbosa1@users.noreply.github.com>
2018-05-09 18:50:02 -0400

    Update SymbolTable.java

--- src/java/SymbolTable.java
+++ src/java/SymbolTable.java
@@ -3,11 +3,11 @@
    * file: SymbolTable.java
    * author: Artur Barbosa
    * course: CMPT 432
-   * assignment: project 3 
-   * due date: 4/3/2018
-   * Version:2
+   * assignment: project 4
+   * due date: 5/8/2018
+   * Version:4
    * 
-   * This file contains the SymbolTable for project 3.
+   * This file contains the SymbolTable for project 4.
    *
    */
 import java.util.ArrayList;
